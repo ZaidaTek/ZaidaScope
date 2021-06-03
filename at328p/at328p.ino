d5715b47fa5cf68eda61e3d1d8d5a79cc34acd4f
Andreas Riebesehl <a.riebesehl@gmail.com>
2021-06-03 15:39:10 +0200

    update to v210603
    - forget to include the updated .ino file in git commit, this version was however already included in latest .zip distribution

--- at328p/at328p.ino
+++ at328p/at328p.ino
@@ -3,77 +3,207 @@
 **** For full license text, please visit: https://creativecommons.org/licenses/by-nd/4.0/legalcode
 ***/
 
-// V210309
+// v210603
 
 #define ZDX_MAX_CH 8
 #define ZDX_PACKET_BUFF 16
 
-#define ZDX_MODE_FAST 0x0
+#define ZDX_MODE_ADC 0x0
 #define ZDX_MODE_MIX 0x1
-#define ZDX_MODE_CTRL 0x2
+#define ZDX_MODE_DIO 0x2
+#define ZDX_MODE_PWM 0x3
 
-#define ZDX_PACKET_EXPECT_FAST 9
-#define ZDX_PACKET_EXPECT_MIX 12
-#define ZDX_PACKET_EXPECT_CTRL 3
+#define ZDX_PACKETS_ADC 9
+#define ZDX_PACKETS_MIX 12
+#define ZDX_PACKETS_DIO 2
+#define ZDX_PACKETS_PWM 6
 
-#define ZDX_DEFAULT_CHCONFIG 0xff
-#define ZDX_DEFAULT_SAMPDLY 1000
-#define ZDX_DEFAULT_CFGMODE 0x0
+unsigned char gADC_Config;
+unsigned long gADC_Timer;
+unsigned char gDIO_Config;
+unsigned char gPWM_Config;
+unsigned int gPWM_Depth;
+unsigned int gPWM_Timer;
 
-
-unsigned char gZDX_Mode = ZDX_DEFAULT_CFGMODE;
-unsigned char gZDX_Packets[ZDX_PACKET_BUFF] = {0};
-
-unsigned char gAI_ChConfig = ZDX_DEFAULT_CHCONFIG;
-unsigned char gAI_ChNum = 0;
-unsigned char gAI_ChPin[ZDX_MAX_CH] = {0};
-unsigned char gAI_ChIndex = 0;
-unsigned char gAI_HiByte = 0;
-unsigned char gAI_LoByte = 0;
-unsigned char gAI_AcquireSample = 0;
-unsigned int gAI_TimerValue = 0;
-unsigned long gAI_SampleDelay = ZDX_DEFAULT_SAMPDLY;
-
-unsigned char gDO_ChConfig = 0x0;
-unsigned char gDO_ChInit = 0x0;
-unsigned char gDO_ChState = 0x0;
-
-void gZDX_Init(void) {
+void ZDX_ADC_Timer(void) {
+	unsigned int lTimer;
+	unsigned char lPrescale;
+	if (gADC_Timer < 0x10000UL) {
+		lPrescale = 0x1;
+		lTimer = gADC_Timer;
+	} else if (gADC_Timer < 0x80000UL) {
+		lPrescale = 0x2;
+		lTimer = gADC_Timer >> 3;
+	} else if (gADC_Timer < 0x400000UL) {
+		lPrescale = 0x3;
+		lTimer = gADC_Timer >> 6;
+	} else if (gADC_Timer < 0x1000000UL) {
+		lPrescale = 0x4;
+		lTimer = gADC_Timer >> 8;
+	} else {
+		lPrescale = 0x5;
+		lTimer = gADC_Timer >> 10;
+	}
+	OCR1A = lTimer;
+	TCCR1A = 0x0;
+	TCCR1B = 0x8 | lPrescale;
+}
+void ZDX_ADC_Clock(unsigned char iChannels) {
+	unsigned long lPrescale = 16UL * 0x2UL * (unsigned long)iChannels;
+	unsigned char i = 0;
+	while ((gADC_Timer > lPrescale) && (i < 7)) {
+		++i;
+		lPrescale <<= 1;
+	}
+	ADCSRB = 0x0;
+	ADCSRA = 0x80 | i;
+}
+void ZDX_ADC_Init(void) {
+	UCSR0B = 0x8;
+	DIDR0 = 0x3f;
+	DIDR1 = 0x3;
+	ADMUX |= 0x40;
+}
+void ZDX_ADC_Main(void) {
+	#define mZDX_ADC_Main_INCREMENT() lTransmit = lRead; if (++lRead == lChannels) {lRead = 0;} ADMUX &= 0xf0; ADMUX |= lPort[lRead]
+	unsigned char lPort[ZDX_MAX_CH];
+	unsigned char lChannels = 0;
+	unsigned char lTransmit;
+	unsigned char lRead = 0;
+	for (unsigned char i = 0; i < ZDX_MAX_CH; i++) {if (gADC_Config & (1 << i)) {lPort[lChannels++] = i;}}
+	ZDX_ADC_Init();
+	ZDX_ADC_Clock(lChannels);
+	ZDX_ADC_Timer();
+	mZDX_ADC_Main_INCREMENT();
+	while(0x1) {
+		unsigned char lAcquire = lChannels;
+		do {
+			if (!(ADCSRA & 0x40)) {
+				unsigned char lSampleL = ADCL;
+				unsigned char lSampleH = ADCH;
+				ADCSRA |= 0x40;
+				while (!(UCSR0A & 0x20));
+				UDR0 = 0x80 | (lTransmit << 3) | (lSampleH << 1) | (lSampleL >> 7);
+				while (!(UCSR0A & 0x20));
+				UDR0 = lSampleL & 0x7f;
+				mZDX_ADC_Main_INCREMENT();
+				--lAcquire;
+			}
+		} while (lAcquire);
+		while (!(TIFR1 & 0x2));
+		TIFR1 |= 0x2;
+	}
+	#undef mZDX_ADC_Main_INCREMENT()
+}
+void ZDX_DIO_Write(unsigned char iState) {
+	iState &= gDIO_Config;
+	unsigned char lPortB = iState & 0x3;
+	unsigned char lPortD = iState & 0xfc;
+	PORTB &= lPortB;
+	PORTD &= lPortD;
+	PORTB |= lPortB;
+	PORTD |= lPortD;
+}
+void ZDX_DIO_Init(void) {
+	DDRB = gDIO_Config & 0x3;
+	DDRD = gDIO_Config & 0xfc;
+}
+void ZDX_DIO_Main(void) {
+	ZDX_DIO_Init();
+	while(0x1) {if (UCSR0A & 0x80) {ZDX_DIO_Write(UDR0);}}
+}
+void ZDX_MIX_Main(void) {
+	while(0x1) {__asm__("nop\n\t");}
+}
+void ZDX_PWM_Init(void) {
+	DDRB = gPWM_Config & 0x3;
+	DDRD = gPWM_Config & 0xfc;
+	OCR1A = gPWM_Timer; // shouldn't this be one less?
+	TCCR1A = 0x0;
+	TCCR1B = 0x9;
+}
+#define mZDX_PWM_MAIN(TYPE) \
+	unsigned char lCache = 0x0;\
+	TYPE lMax = gPWM_Depth;\
+	TYPE lPhase = lMax;\
+	TYPE lDuty[8] = {0x0};\
+	ZDX_PWM_Init();\
+	while (0x1) {\
+		if (TIFR1 & 0x2) {\
+			TIFR1 |= 0x2;\
+			++lPhase;\
+			lPhase &= lMax;\
+			unsigned char lOut;\
+			for (unsigned char i = 8; i;) {lOut <<= 1; lOut |= (lDuty[--i] >= lPhase);}\
+			lOut &= gPWM_Config;\
+			PORTB = lOut & 0x3;\
+			PORTD = lOut & 0xfc;\
+		}\
+		if (UCSR0A & 0x80) {\
+			unsigned char lBuffer = UDR0;\
+			if (lCache) {\
+				if (!(lBuffer & 0x80)) {lDuty[(lCache >> 3) & 0x7] = ((TYPE)(lCache & 0x7) << 7) | (TYPE)lBuffer;}\
+				lCache = 0x0;\
+			} else if (lBuffer & 0x80) {\
+				lCache = lBuffer;\
+			}\
+		}\
+	}
+void ZDX_PWM8_Main(void) {mZDX_PWM_MAIN(unsigned char);}
+void ZDX_PWM16_Main(void) {mZDX_PWM_MAIN(unsigned int);}
+#undef mZDX_PWM_MAIN()
+void ZDX_PWM_Main(void) {
+	if (gPWM_Depth > 0xff) {
+		ZDX_PWM16_Main();
+	} else {
+		ZDX_PWM8_Main();
+	}
+}
+void ZDX_Init(void) {
 	UBRR0L = 0x0;
 	UBRR0H = 0x0;
 	UCSR0A = 0x2;
 	UCSR0B = 0x18;
 	UCSR0C = 0x6;
 }
-void gZDX_AwaitConfig(void) {
+unsigned char ZDX_Mode(void) {
+	unsigned char lBuffer[ZDX_PACKET_BUFF];
 	unsigned char lReceived = 0;
 	unsigned char lWait = 0x1;
+	unsigned char lMode = 0xff;
 	while (lWait) {
 		if (UCSR0A & 0x80) {
-			gZDX_Packets[lReceived] = UDR0;
-			if (++lReceived == 1) {gZDX_Mode = gZDX_Packets[0];}
-			switch (gZDX_Mode) {
-				case ZDX_MODE_FAST:
-					if (lReceived == ZDX_PACKET_EXPECT_FAST) {
-						gAI_SampleDelay = (long)gZDX_Packets[1] << 24;
-						gAI_SampleDelay |= (long)gZDX_Packets[2] << 16;
-						gAI_SampleDelay |= (long)gZDX_Packets[3] << 8;
-						gAI_SampleDelay |= gZDX_Packets[4];
-						gAI_ChConfig = gZDX_Packets[8];
-						gAI_AcquireSample = 0x1;
+			lBuffer[lReceived++] = UDR0;
+			if (lReceived == 1) {lMode = lBuffer[0];}
+			switch (lMode) {
+				case ZDX_MODE_ADC:
+					if (lReceived == ZDX_PACKETS_ADC) {
+						gADC_Timer  = (unsigned long)lBuffer[1] << 24;
+						gADC_Timer |= (unsigned long)lBuffer[2] << 16;
+						gADC_Timer |= (unsigned long)lBuffer[3] << 8;
+						gADC_Timer |= (unsigned long)lBuffer[4];
+						gADC_Config = lBuffer[8];
 						lWait = 0x0;
 					}
 					break;
 				case ZDX_MODE_MIX:
-					if (lReceived == ZDX_PACKET_EXPECT_MIX) {
+					if (lReceived == ZDX_PACKETS_MIX) {
 						lWait = 0x0;
 					}
 					break;
-				case ZDX_MODE_CTRL:
-					if (lReceived == ZDX_PACKET_EXPECT_CTRL) {
-						gDO_ChConfig = gZDX_Packets[1];
-						gDO_ChInit = gZDX_Packets[2];
-						gDO_ChState = gDO_ChInit;
+				case ZDX_MODE_DIO:
+					if (lReceived == ZDX_PACKETS_DIO) {
+						gDIO_Config = lBuffer[1];
+						lWait = 0x0;
+					}
+					break;
+				case ZDX_MODE_PWM:
+					if (lReceived == ZDX_PACKETS_PWM) {
+						gPWM_Config = lBuffer[1];
+						gPWM_Timer  = (unsigned int)lBuffer[2] << 8;
+						gPWM_Timer |= (unsigned int)lBuffer[3];
+						gPWM_Depth  = (unsigned int)lBuffer[4] << 8;
+						gPWM_Depth |= (unsigned int)lBuffer[5];
 						lWait = 0x0;
 					}
 					break;
@@ -85,145 +215,16 @@ void gZDX_AwaitConfig(void) {
 			__asm__("nop\n\t");
 		}
 	}
+	return lMode;
 }
-void gZDX_SetAITimer(void) {
-	TCCR1A = 0x0;
-	if (gAI_SampleDelay < 0x10000) {
-		TCCR1B = 0x1;
-		gAI_TimerValue = gAI_SampleDelay;
-		gAI_TimerValue -= 0xD;
-	} else if (gAI_SampleDelay < 0x80000) {
-		TCCR1B = 0x2;
-		gAI_TimerValue = gAI_SampleDelay >> 3;
-		gAI_TimerValue -= 0x1;
-	} else if (gAI_SampleDelay < 0x400000) {
-		TCCR1B = 0x3;
-		gAI_TimerValue = gAI_SampleDelay >> 6;
-	} else if (gAI_SampleDelay < 0x1000000) {
-		TCCR1B = 0x4;
-		gAI_TimerValue = gAI_SampleDelay >> 8;
-	} else {
-		TCCR1B = 0x5;
-		gAI_TimerValue = gAI_SampleDelay >> 10;
-	}
-	TCNT1 = 0;
-}
-void gZDX_SetADCClock(void) {
-	unsigned long lPrescale = 0x2;
-	unsigned char i = 0;
-	while (gAI_SampleDelay > (14 * lPrescale * gAI_ChNum) && i < 7) {
-		++i;
-		lPrescale <<= 1;
-	}
-	ADCSRB = 0x0;
-	ADCSRA = (0x80 | i);
-}
-void gZDX_SetupDO(void) {
-	DDRD &= 0x3;
-	DDRD |= ((gDO_ChConfig & 0x3f) << 2);
-	DDRB &= 0xfc;
-	DDRB |= ((gDO_ChConfig & 0xc0) >> 6);
-}
-
-void gZDX_WriteDO(void) {
-	unsigned char prototypeD = ((gDO_ChState & 0x3f) << 2);
-	unsigned char currentD = PORTD & 0xfc;
-	unsigned char toggleD = prototypeD ^ currentD;
-	for (unsigned char i = 2; i < 8; i++) {
-		unsigned char bitIndex = (1 << i);
-		if (toggleD & bitIndex) {
-			if (prototypeD & bitIndex) {
-				PORTD |= bitIndex;
-			} else {
-				PORTD &= ~bitIndex;
-			}
-		}
-	}
-	unsigned char prototypeB = ((gDO_ChState & 0xc0) >> 6);
-	unsigned char currentB = PORTB & 0x3;
-	unsigned char toggleB = prototypeB ^ currentB;
-	for (unsigned char i = 0; i < 2; i++) {
-		unsigned char bitIndex = (1 << i);
-		if (toggleB & bitIndex) {
-			if (prototypeB & bitIndex) {
-				PORTB |= bitIndex;
-			} else {
-				PORTB &= ~bitIndex;
-			}
-		}
-	}
-}
-
-void gZDX_Main_Fast(void) {
-	while(0x1) {
-		while(gAI_AcquireSample) {
-			if (ADCSRA & 0x40) {
-				if ((ADMUX & 0xf) == gAI_ChPin[gAI_ChIndex]) {
-					gAI_ChIndex = (gAI_ChIndex ? gAI_ChIndex : gAI_ChNum) - 1;
-					gZDX_Packets[0] = 0x80 | (gAI_ChIndex << 3);
-					gZDX_Packets[0] |= (gAI_HiByte << 1) | (gAI_LoByte >> 7);
-					while (!(UCSR0A & 0x20));
-					UDR0 = gZDX_Packets[0];
-					while (!(UCSR0A & 0x20));
-					UDR0 = (gAI_LoByte & 0x7f);
-					--gAI_AcquireSample;
-				}
-			} else {
-				gAI_LoByte = ADCL;
-				gAI_HiByte = ADCH;
-				gAI_ChIndex = (gAI_ChNum - gAI_AcquireSample) & 0x7;
-				ADMUX = 0x40 | gAI_ChPin[gAI_ChIndex];
-				ADCSRA |= 0x40;
-			}
-		}
-		while (!(TIFR1 & 0x1));
-		TIFR1 |= 0x1;
-		TCNT1 -= gAI_TimerValue;
-		gAI_AcquireSample = gAI_ChNum;
-	}
-}
-
-void gZDX_Main_Mix(void) {
-	while(0x1) {
-		__asm__("nop\n\t");  
-	}
-}
-
-void gZDX_Main_Ctrl(void) {
-	while(0x1) {
-		if (UCSR0A & 0x80) {gDO_ChState = UDR0;}
-		gZDX_WriteDO();
-	}
-}
-
-
 int main() {
-	gZDX_Init();
-	gZDX_AwaitConfig();
-	switch (gZDX_Mode) {
-		case ZDX_MODE_MIX:
-			gZDX_Main_Mix();
-			break;
-		case ZDX_MODE_CTRL:
-			gZDX_SetupDO();
-			gZDX_Main_Ctrl();
-			break;
-		default:
-		case ZDX_MODE_FAST:
-			UCSR0B = 0x8;
-			for (unsigned char i = 0; i < ZDX_MAX_CH; i++) {
-				if (gAI_ChConfig & (1 << i)) {
-				  gAI_ChPin[gAI_ChNum] = (i & 0x7);
-				  ++gAI_ChNum;
-				}
-			}
-			DIDR0 = 0xff;
-			DIDR1 = 0x3;
-			gZDX_SetADCClock();
-			gZDX_SetAITimer();
-			gAI_AcquireSample = gAI_ChNum;
-			gZDX_Main_Fast();
-			break;
+	ZDX_Init();
+	switch (ZDX_Mode()) {
+		case ZDX_MODE_ADC: ZDX_ADC_Main(); break;
+		case ZDX_MODE_MIX: ZDX_MIX_Main(); break;
+		case ZDX_MODE_DIO: ZDX_DIO_Main(); break;
+		case ZDX_MODE_PWM: ZDX_PWM_Main(); break;
+		default: break;
 	}
-	return 0;
+	return 0x0;
 }
